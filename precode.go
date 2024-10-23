214e126e2467435a142bec858000871bfe7403d3
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2024-10-23 21:47:47 +0300

    The end

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -40,13 +42,91 @@ var tasks = map[string]Task{
 }
 
 // Ниже напишите обработчики для каждого эндпоинта
-// ...
+
+// эндпоинт для получения всех задач
+func getTasks(rw http.ResponseWriter, _ *http.Request) {
+	// сериализуем данные из мапы tasks
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// в заголовок записываем тип контента в формате JSON
+	rw.Header().Set("Content-Type", "application/json")
+	//  статус OK
+	rw.WriteHeader(http.StatusOK)
+	//записываем сериализованные данные в тело ответа в формате JSON
+	rw.Write(resp)
+}
+
+// эндпоинт для отправки задачи на сервер
+func postTask(rw http.ResponseWriter, req *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(req.Body) // читаем поток данных
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+	//десериализация данных
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tasks[task.ID] = task
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusCreated)
+
+}
+
+// эндпоинт для получения задачи по ID
+func getTaskID(rw http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(rw, "Задача не найдена", http.StatusNoContent)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(resp)
+}
+
+func dellTaskID(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+
+	taskId := chi.URLParam(req, "id")
+
+	if _, ok := tasks[taskId]; !ok {
+		http.Error(rw, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+	delete(tasks, taskId)
+	rw.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	// Обработчик для получения всех задач
+	r.Get("/tasks", getTasks)
+	//Обработчик для отправки задачи на сервер
+	r.Post("/tasks", postTask)
+	//Обработчик для получения задачи по ID
+	r.Get("/task/{id}", getTaskID)
+	//Обработчик удаления задачи по ID
+	r.Delete("/task/{id}", dellTaskID)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
