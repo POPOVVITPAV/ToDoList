4a8ba9d5b98d09bf663e71f26e042abdda25cfc5
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2024-10-27 20:00:15 +0300

    Second iteration

--- precode.go
+++ precode.go
@@ -14,7 +14,7 @@ type Task struct {
 	ID           string   `json:"id"`
 	Description  string   `json:"description"`
 	Note         string   `json:"note"`
-	Applications []string `json:"applications, omitempty"`
+	Applications []string `json:"applications"`
 }
 
 var tasks = map[string]Task{
